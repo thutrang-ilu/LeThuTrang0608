d2a570a95733caeae75116ff93fe1fe9e2e6d856
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-04-25 13:35:17 +0700

    2 commit

---
+++ LeThuTrang0608/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeThuTrang0608.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
