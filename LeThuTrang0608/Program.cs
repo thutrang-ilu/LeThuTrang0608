836d8764cc20c1abde4e94ef141e8103299231b2
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-04-25 14:03:50 +0700

    lan 2

--- LeThuTrang0608/Program.cs
+++ LeThuTrang0608/Program.cs
@@ -1,10 +1,13 @@
+using LeThuTrang0608.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
